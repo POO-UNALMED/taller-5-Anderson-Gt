1a08605e5c316902b08ac98a91f4ac41e7841eaa
Anderson Gutierrez <69688234+Anderson-Gt@users.noreply.github.com>
2020-09-29 23:46:16 -0500

    taller

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -22,14 +22,16 @@ public class Animal {
 	}
 	
 	public String toString() {
-		if (zona==null) {
-			return "Mi nombre es"+nombre+",tengo una edad de "+edad+", habito en "+habitat+" y mi genero es "+genero;
+		if (this.getZona() != null){
+			return "Mi nombre es "+this.getNombre()+", tengo una edad de "+this.getEdad()+", habito en "+this.getHabitat()+
+					" y mi genero es "+this.getGenero()+", la zona en la que me ubico es "+this.zona.getNombre()+", en el "
+					+this.zona.getZoo().getNombre();
 		}
 		else {
-			return "Mi nombre es"+nombre+",tengo una edad de "+edad+", habito en "+habitat+" y mi genero es "+genero+
-					", la zona en la que me ubico es "+zona+" en el "+ zona.getZoo().getNombre();
+			return "Mi nombre es "+ this.getNombre()+ ", tengo una edad de "+this.getEdad()
+			+", habito en "+this.getHabitat()+" y mi genero es "+ this.getGenero();
 		}
-		
+			
 	}
 	
 	public Animal(String nombre,int edad,String habitat, String genero) {
